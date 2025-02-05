212d7cf231a7f6963e05b8ab1f9b4cfbf614e314
MAKEYI <142371726+jinyulv-110@users.noreply.github.com>
2025-02-05 23:24:28 +0800

    Add files via upload

--- data encapsulation.cpp
+++ data encapsulation.cpp
@@ -1,36 +1,57 @@
-//数据封装
 #include <iostream>
 using namespace std;
- 
-class Adder{
-   public:
-      // 构造函数
-      Adder(int i = 0)
-      {
-        total = i;
-      }
-      // 对外的接口
-      void addNum(int number)
-      {
-          total += number;
-      }
-      // 对外的接口
-      int getTotal()
-      {
-          return total;
-      };
-   private:
-      // 对外隐藏的数据
-      int total;
+
+class Student {
+private:
+    string name;
+    int age;
+
+public:
+    // 构造函数 
+    Student(string studentName, int studentAge) {
+        name = studentName;
+        age = studentAge;
+    }
+
+    // 访问器函数(getter)
+    string getName() {
+        return name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    // 修改器函数(setter)
+    void setName(string studentName) {
+        name = studentName;
+    }
+
+    void setAge(int studentAge) {
+        if (studentAge > 0) {
+            age = studentAge;
+        } else {
+            cout << "Invalid age!" << endl;
+        }
+    }
+
+    // 打印学生信息 
+    void printInfo() {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
 };
-int main( )
-{
-   Adder a;
-   
-   a.addNum(10);
-   a.addNum(20);
-   a.addNum(30);
- 
-   cout << "Total " << a.getTotal() <<endl;
-   return 0;
+
+int main() {
+    //创建一个 Student 对象
+    Student student1("Alice", 20);
+
+    //访问和修改数据 
+    student1.printInfo();
+
+    student1.setName("Bob");
+    student1.setAge(22);
+
+    student1.printInfo();
+
+    return 0;
 }
